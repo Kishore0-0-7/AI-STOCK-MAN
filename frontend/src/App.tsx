d7d3aae21b5ebe1bf2148d37e24b61d8df82e75f
girinataraj <girinataraj765@gmail.com>
2025-08-13 14:15:41 +0530

    Modified the app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
-import Billing from "./pages/Billing";
 import PurchaseOrders from "./pages/PurchaseOrders";
-import CustomerOrders from "./pages/CustomerOrders";
-import ScanBills from "./pages/ScanBills";
 import SalesReports from "./pages/SalesReports";
 import StockSummary from "./pages/StockSummary";
 import LowStockAlerts from "./pages/LowStockAlerts";
@@ -17,7 +14,6 @@ import Settings from "./pages/Settings";
 import Suppliers from "./pages/Suppliers";
 import Customers from "./pages/Customers";
 import NotFound from "./pages/NotFound";
-import ApiTestComponent from "./components/ApiTestComponent";
 
 const queryClient = new QueryClient();
 
@@ -33,15 +29,15 @@ const App = () => (
             <Route path="products" element={<Products />} />
             {/* <Route path="billing" element={<Billing />} /> */}
             <Route path="purchase-orders" element={<PurchaseOrders />} />
-            <Route path="customer-orders" element={<CustomerOrders />} />
-            <Route path="scan-bills" element={<ScanBills />} />
-            <Route path="sales-reports" element={<SalesReports />} />
+            {/* <Route path="customer-orders" element={<CustomerOrders />} /> */}
+            {/* <Route path="scan-bills" element={<ScanBills />} /> */}
+            {/* <Route path="sales-reports" element={<SalesReports />} /> */}
             <Route path="stock-summary" element={<StockSummary />} />
             <Route path="low-stock-alerts" element={<LowStockAlerts />} />
             <Route path="suppliers" element={<Suppliers />} />
             <Route path="customers" element={<Customers />} />
             <Route path="settings" element={<Settings />} />
-            <Route path="api-test" element={<ApiTestComponent />} />
+            {/* <Route path="api-test" element={<ApiTestComponent />} /> */}
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
